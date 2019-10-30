12faf4260d9a6fc8ede50b45b4314304211333c2
Zabana <karim.cheurfi@gmail.com>
2019-10-30 10:13:41 +0100

    added utils package and version option to the cli

--- wypy/cli.py
+++ wypy/cli.py
@@ -1,8 +1,10 @@
 import click
 from wypy.general.commands import general as general_commands
+from wypy.utils.constants import VERSION
 
 
 @click.group()
+@click.version_option(version=VERSION, message=f'WyPy - Version {VERSION}')
 def cli():
     """
     wypy is command line NetworkManager utility.
