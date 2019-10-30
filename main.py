278235cc41486d8f26a9a885e5dd87c598647e21
Zabana <karim.cheurfi@gmail.com>
2019-10-30 14:14:40 +0100

    whatever

--- main.py
+++ main.py
@@ -8,19 +8,44 @@ NM_BUS_OBJ = '/org/freedesktop/NetworkManager'
 bus = dbus.SystemBus()
 
 # get the network manager object (proxy)
+
+props = [
+    'State', 'WirelessEnabled',
+    'WirelessHardwareEnabled', 'WwanEnabled',
+    'WwanHardwareEnabled'
+]
+
 nm = bus.get_object(NM_BUS_NAME, NM_BUS_OBJ)
 
-devices = nm.Get(
-    NM_BUS_NAME,
-    'AllDevices',
-    dbus_interface=dbus.PROPERTIES_IFACE
-)
 
-for device in devices:
-    dev = bus.get_object(NM_BUS_NAME, device)
-    dev_name = dev.Get(
-        NM_BUS_NAME + '.Device',
-        'Interface',
+def get_dbus_object_property(prop_name):
+    # maybe there should be a try/except
+
+    return nm.Get(
+        NM_BUS_NAME,
+        prop_name,
         dbus_interface=dbus.PROPERTIES_IFACE
     )
-    print(dev_name)
+
+
+state = [get_dbus_object_property(prop) for prop in props]
+# state = nm.state(dbus_interface=NM_IFACE)
+for prop in state:
+    print(prop)
+
+# devices = nm.Get(
+#     NM_BUS_NAME,
+#     'WirelessEnabled',
+#     dbus_interface=dbus.PROPERTIES_IFACE
+# )
+
+# print(devices)
+
+# for device in devices:
+#     dev = bus.get_object(NM_BUS_NAME, device)
+#     dev_name = dev.Get(
+#         NM_BUS_NAME + '.Device',
+#         'Interface',
+#         dbus_interface=dbus.PROPERTIES_IFACE
+#     )
+#     print(dev_name)
