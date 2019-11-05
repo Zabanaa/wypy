a23f58e0572f1537390036756b9f2d8eacf9db34
Zabana <karim.cheurfi@gmail.com>
2019-11-05 17:16:37 +0100

    rename NM_SETTINGS_BUS_NAME to NM_SETTINGS_OBJ_PATH and added NM_ACTIVE_CONN_IFACE

--- wypy/utils/constants.py
+++ wypy/utils/constants.py
@@ -2,13 +2,15 @@ VERSION = '0.1.0'
 NM_BUS_NAME = 'org.freedesktop.NetworkManager'
 
 NM_OBJ_PATH = '/org/freedesktop/NetworkManager'
-NM_SETTINGS_OBJ_PATH = '/org/freedesktop/NetworkManager/Settings'
 
-NM_SETTINGS_BUS_NAME = 'org.freedesktop.NetworkManager.Settings'
+NM_SETTINGS_OBJ_PATH = '/org/freedesktop/NetworkManager/Settings'
 NM_SETTINGS_IFACE = 'org.freedesktop.NetworkManager.Settings'
+
 NM_IFACE = 'org.freedesktop.NetworkManager'
 
 NM_DEVICE_IFACE = 'org.freedesktop.NetworkManager.Device'
+
+NM_ACTIVE_CONN_IFACE = 'org.freedesktop.NetworkManager.Connection.Active'
 
 DBUS_GENERAL_PROPS = {
     'Connectivity': 'CONNECTIVITY',
