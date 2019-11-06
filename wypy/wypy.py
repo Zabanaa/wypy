927615becc360f8d542085ba5621306d8fb2978c
Zabana <karim.cheurfi@gmail.com>
2019-11-06 11:31:04 +0100

    added translate_device_type method to WyPy

--- wypy/wypy.py
+++ wypy/wypy.py
@@ -89,3 +89,94 @@ class WyPy(object):
                 return colored('enabled', 'green')
             if code == 0:
                 return colored('disabled', 'red')
+
+    def translate_device_type(self, _type):
+        if _type == 30:
+            return 'WiFi P2P'
+
+        if _type == 29:
+            return 'WireGuard'
+
+        if _type == 28:
+            return '6LoWPAN'
+
+        if _type == 27:
+            return 'WPAN'
+
+        if _type == 26:
+            return 'Open vSwitch Bridge'
+
+        if _type == 25:
+            return 'Openv Switch Port'
+
+        if _type == 24:
+            return 'Openv Switch Interface'
+
+        if _type == 23:
+            return 'PPP'
+
+        if _type == 22:
+            return 'Dummy'
+
+        if _type == 21:
+            return 'MACsec'
+
+        if _type == 20:
+            return 'VETH'
+
+        if _type == 19:
+            return 'VXLAN'
+
+        if _type == 18:
+            return 'MACVLAN'
+
+        if _type == 17:
+            return 'IP tunnel'
+
+        if _type == 16:
+            return 'TUN/TAP'
+
+        if _type == 15:
+            return 'Team Master'
+
+        if _type == 14:
+            return 'Generic'
+
+        if _type == 13:
+            return 'Bridge'
+
+        if _type == 12:
+            return 'ADSL Modem'
+
+        if _type == 11:
+            return 'VLAN'
+
+        if _type == 10:
+            return 'Bond'
+
+        if _type == 9:
+            return 'IP Infiniband'
+
+        if _type == 8:
+            return 'Modem'
+
+        if _type == 7:
+            return 'WiMax'
+
+        if _type == 6:
+            return 'OLPC XO Mesh'
+
+        if _type == 5:
+            return 'Bluetooth'
+
+        if _type in [4, 3]:
+            return 'Unused'
+
+        if _type == 2:
+            return 'WiFi'
+
+        if _type == 1:
+            return 'Ethernet'
+
+        if _type == 0:
+            return 'Unknown'
