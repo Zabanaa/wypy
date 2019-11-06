832e9a4004d7dd878cc728e75462a78571e75358
Zabana <karim.cheurfi@gmail.com>
2019-11-06 17:18:30 +0100

    list all connections + some housekeeping

--- wypy/connection/connection.py
+++ wypy/connection/connection.py
@@ -1,3 +1,4 @@
+from wypy.utils.helpers import flatten
 from prettytable import PrettyTable
 from termcolor import colored
 from wypy.utils.constants import (
@@ -6,7 +7,8 @@ from wypy.utils.constants import (
     NM_SETTINGS_IFACE,
     NM_SETTINGS_OBJ_PATH,
     NM_ACTIVE_CONN_IFACE,
-    NM_DEVICE_IFACE
+    NM_DEVICE_IFACE,
+    NM_CONNECTION_IFACE
 )
 from wypy.wypy import WyPy
 import click
@@ -20,7 +22,11 @@ class Connection(WyPy):
         self.proxy = self.bus.get_object(NM_BUS_NAME, NM_OBJ_PATH)
         self.active_conns_prop = 'ActiveConnections'
         self.conn_props = ['Id', 'Uuid', 'Type', 'Devices']
-        self.table = PrettyTable(['Name', 'UUID', 'TYPE', 'DEVICE'])
+        self.table = PrettyTable(['NAME', 'UUID', 'TYPE', 'DEVICE'])
+        self.table.align = 'l'
+        self.active_connections = []
+        self.settings_obj = self.bus.get_object(NM_BUS_NAME, NM_SETTINGS_OBJ_PATH)  # noqa E501
+        self.settings_iface = dbus.Interface(self.settings_obj, NM_SETTINGS_IFACE)  # noqa E501
 
     def activate(self):
         click.echo('Activating connection ...')
@@ -30,15 +36,32 @@ class Connection(WyPy):
 
     def show_all(self):
         click.echo('Showing all connections ...')
-        pass
+        self._get_active_connections()
+        connections = self.settings_iface.ListConnections()
 
-    def show_one(self):
-        click.echo('Connection details: hehe')
+        for conn in connections:
+            conn_obj = self.bus.get_object(NM_BUS_NAME, conn)
+            conn_iface = dbus.Interface(conn_obj,  NM_CONNECTION_IFACE)
+            conn_info = conn_iface.GetSettings()['connection']
+
+            conn_data = {
+                'id': str(conn_info.get('id', '')),
+                'uuid': str(conn_info.get('uuid', '')),
+                'type': conn_info.get('type', ''),
+                'name': str(conn_info.get('interface-name', '--')),
+            }
+
+            if self._is_connection_active(conn_data['uuid']):
+                self._color_data(conn_data)
+
+            self.table.add_row(conn_data.values())
+
+        click.echo(self.table)
 
     def show_active(self):
         click.echo('Showing active connections ...')
-        conns = self._get_active_connections()
-        conn_values = [conn.values() for conn in conns]
+        self._get_active_connections()
+        conn_values = [conn.values() for conn in self.active_connections]
 
         for i, vals in enumerate(conn_values):
             conn_values[i] = list(map(lambda val: colored(val, "green"), vals))
@@ -50,8 +73,6 @@ class Connection(WyPy):
             proxy=self.proxy,
             prop_name=self.active_conns_prop
         )
-
-        active_conns_data = []
 
         for conn in active_conns_paths:
             all_props = self.get_all_properties(conn, NM_ACTIVE_CONN_IFACE)
@@ -65,9 +86,7 @@ class Connection(WyPy):
             conn_props['Device'] = device_name
             conn_props['Type'] = self.translate_device_type(device_type)
 
-            active_conns_data.append(conn_props)
-
-        return active_conns_data
+            self.active_connections.append(conn_props)
 
     def _get_device_info(self, connection_props):
         device_obj = str(connection_props['Devices'][0])
@@ -78,3 +97,11 @@ class Connection(WyPy):
         for row in data:
             self.table.add_row(row)
         click.echo(self.table)
+
+    def _is_connection_active(self, uuid):
+        active_conn_uuids = [conn['Uuid'] for conn in self.active_connections]
+        return uuid in active_conn_uuids
+
+    def _color_data(self, conn):
+        for key, val in conn.items():
+            conn[key] = colored(val, "green")
